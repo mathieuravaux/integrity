ab07035b056165f1d4978022cb2d7ec1be5f3ca7
Chris Wanstrath <chris@ozmm.org>
2008-11-16 04:03:39 -0800

    Delete all notifiers for this project on update - not all notifiers for every project.

--- lib/integrity/notifier.rb
+++ lib/integrity/notifier.rb
@@ -1,39 +1,39 @@
 module Integrity
   class Notifier
     include DataMapper::Resource
-    
+
     property :id,      Integer,  :serial => true
     property :name,    String,   :nullable => false
     property :config,  Yaml,     :nullable => false, :lazy => false
-    
+
     belongs_to :project, :class_name => "Integrity::Project"
-    
+
     validates_is_unique :name, :scope => :project_id
     validates_present :project_id
-    
+
     def self.available
       @available ||= constants.map {|name| const_get(name) }.select do |notifier|
         notifier.respond_to?(:to_haml) && notifier.respond_to?(:notify_of_build)
       end - [Notifier::Base]
     end
-    
+
     def self.enable_notifiers(project, enabled, config={})
-      all.destroy!
+      Project.get(project).notifiers.destroy!
       list_of_enabled_notifiers(enabled).each do |name|
         create! :project_id => project, :name => name, :config => config[name]
       end
     end
-    
+
     def notify_of_build(build)
       to_const.notify_of_build(build, config)
     end
-    
+
     private
-      
+
       def to_const
         self.class.module_eval(name)
       end
-      
+
       def self.list_of_enabled_notifiers(names)
         case names
           when Array then names
